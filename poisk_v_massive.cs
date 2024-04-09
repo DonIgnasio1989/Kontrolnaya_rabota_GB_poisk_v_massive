6e8743ae5e8328a438180705c92306da99ac2bc9
Donignasio1989 <donignasio@vk.com>
2024-04-09 22:24:16 +0300

    add_Rock_names

--- poisk_v_massive.cs
+++ poisk_v_massive.cs
@@ -1,58 +1,58 @@
 using System;
 
-class Program
+internal class Program
 {
-    static void Main(string[] args)
+    private static void Main(string[] args)
     {
         // Задаем начальный массив строк
-        string[] initialArray = { "apple", "banana", "cat", "dog", "elephant", "fox" };
+        string[] initialArray = ["Rammstein", "Пёс", "GorrilaZ", "Цой", "Алиса", "Nightwish", "ДДТ"];
 
         // Выводим начальный массив
-        Console.WriteLine("Initial array:");
+        Console.WriteLine("Рок группы и исполнители:");
         PrintArray(initialArray);
 
         // Получаем новый массив строк длиной не более 3 символов
         string[] newArray = FilterArray(initialArray);
 
         // Выводим новый массив
-        Console.WriteLine("\nNew array with strings of length <= 3:");
+        Console.WriteLine("\nРок группы и исполнители из 3-х букв:");
         PrintArray(newArray);
-    }
 
-    // Метод для фильтрации массива строк
-    static string[] FilterArray(string[] array)
-    {
-        int count = 0;
-        // Определяем количество строк, которые удовлетворяют условию
-        foreach (string str in array)
+        // Метод для фильтрации массива строк
+        string[] FilterArray(string[] array)
         {
-            if (str.Length <= 3)
+            int count = 0;
+            // Определяем количество строк, которые удовлетворяют условию
+            foreach (string str in array)
             {
-                count++;
+                if (str.Length <= 3)
+                {
+                    count++;
+                }
             }
+
+            // Создаем новый массив с учетом найденного количества строк
+            string[] result = new string[count];
+            int index = 0;
+            // Заполняем новый массив строками, удовлетворяющими условию
+            foreach (string str in array)
+            {
+                if (str.Length <= 3)
+                {
+                    result[index++] = str;
+                }
+            }
+
+            return result;
         }
 
-        // Создаем новый массив с учетом найденного количества строк
-        string[] result = new string[count];
-        int index = 0;
-        // Заполняем новый массив строками, удовлетворяющими условию
-        foreach (string str in array)
+        // Метод для вывода массива строк в консоль
+        void PrintArray(string[] array)
         {
-            if (str.Length <= 3)
+            foreach (string str in array)
             {
-                result[index++] = str;
+                Console.WriteLine(str);
             }
-        }
-
-        return result;
-    }
-
-    // Метод для вывода массива строк в консоль
-    static void PrintArray(string[] array)
-    {
-        foreach (string str in array)
-        {
-            Console.WriteLine(str);
         }
     }
 }
